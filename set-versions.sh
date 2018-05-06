f5aea53c58106a13e44efaba23e8b9580f303ff8
Fábio Souza <fabio.souza.eti@gmail.com>
2018-05-06 04:19:16 -0300

    set-versions.sh

--- set-versions.sh
+++ set-versions.sh
@@ -12,7 +12,7 @@ function set_release_version(){
    VERSION="$(get_artifact_version_without_classifier)-RELEASE"
    mvn versions:set -DnewVersion=$VERSION  
    git add pom.xml
-   git commit -m 'Generating release version '$VERSION
+   git commit -m '[skip ci]Generating release version '$VERSION
    git tag -a "v$VERSION" -m "Tagging version v$VERSION"
 }
 
@@ -23,14 +23,14 @@ function set_development_and_increment_version(){
    VERSION="${a[0]}.${a[1]}.${a[2]}-SNAPSHOT"   
    mvn versions:set -DnewVersion=$VERSION
    git add pom.xml 
-   git commit -m 'Setting develop version to '$VERSION   
+   git commit -m '[skip ci]Setting develop version to '$VERSION   
 }
 
 
 
 function push(){
    last_tag=$(git describe --abbrev=0 --tags)
-   git push "https://$GITHUB_TOKEN@github.com/fabiosoaza/spring-rest-base" master
+   git push "https://${GITHUB_TOKEN}@github.com/fabiosoaza/spring-rest-base" master
   # git push "https://$GITHUB_TOKEN@github.com/fabiosoaza/spring-rest-base" $last_tag
    git pull
 
