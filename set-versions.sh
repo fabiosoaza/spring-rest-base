e3e535f6e963f9d9d22ab5ddb63b090de9a84709
Fábio Souza <fabio.souza.eti@gmail.com>
2018-05-06 02:45:32 -0300

    Modified version generation

--- set-versions.sh
+++ set-versions.sh
@@ -1,39 +1,50 @@
 #!/bin/bash
 
-ARTIFACT_VERSION=$(grep -E -m 1 -o "<version>(.*)</version>" pom.xml | sed 's/<version>//' | sed 's/<\/version>//')
-VERSION_WITHOUT_QUALIFIER="${ARTIFACT_VERSION%%-*}"
 
+function get_artifact_version_without_classifier(){
+    ARTIFACT_VERSION=$(grep -E -m 1 -o "<version>(.*)</version>" pom.xml | sed 's/<version>//' | sed 's/<\/version>//')
+    VERSION_WITHOUT_QUALIFIER="${ARTIFACT_VERSION%%-*}"
+    echo $VERSION_WITHOUT_QUALIFIER
+}
 
 
 function set_release_version(){
-   VERSION=$VERSION_WITHOUT_QUALIFIER-RELEASE
-   mvn versions:set -DnewVersion=$VERSION
+   VERSION="$(get_artifact_version_without_classifier)-RELEASE"
+   mvn versions:set -DnewVersion=$VERSION  
    git add pom.xml
    git commit -m 'Generating release version '$VERSION
    git tag -a "v$VERSION" -m "Tagging version v$VERSION"
-   git push --atomic "https://$GITHUB_TOKEN@github.com/fabiosoaza/spring-rest-base" master "$VERSION" > /dev/null 2>&1
-   git pull
-  
 }
 
-function set_development_and_increment_version(){   
+function set_development_and_increment_version(){
+   VERSION_WITHOUT_QUALIFIER="$(get_artifact_version_without_classifier)"   
    a=( ${VERSION_WITHOUT_QUALIFIER//./ } )                  
    ((a[1]++))            
-   NEW_VERSION="${a[0]}.${a[1]}.${a[2]}"   
-   mvn versions:set -DnewVersion=$NEW_VERSION-SNAPSHOT
-   git add pom.xml
-   git commit -m 'Setting release version to '$NEW_VERSION-SNAPSHOT
-   git push "https://$GITHUB_TOKEN@github.com/fabiosoaza/spring-rest-base" master > /dev/null 2>&1
-   git pull
-  
+   VERSION="${a[0]}.${a[1]}.${a[2]}-SNAPSHOT"   
+   mvn versions:set -DnewVersion=$NEW_VERSION-SNAPSHOT 
+   git add pom.xml 
+   git commit -m 'Setting develop version '$NEW_VERSION-SNAPSHOT   
 }
 
 
-case $1 in
-"develop")
-    set_development_and_increment_version
-    ;;
-"release")
-    set_release_version
-    ;;
-esac
+
+function push(){
+   last_tag=$(git describe --abbrev=0 --tags)
+   git push "https://$GITHUB_TOKEN@github.com/fabiosoaza/spring-rest-base" master $last_tag > /dev/null 2>&1
+   git pull
+
+}
+
+ case $1 in
+        "develop")
+            set_development_and_increment_version
+            ;;
+        "release")
+            set_release_version
+            ;;
+        "push")
+            push
+            ;;   
+  esac
+
+
