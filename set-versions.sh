4fc7ca704e75e87ca01f4aacbfd20ca4bd254db8
Fábio Souza <fabio.souza.eti@gmail.com>
2018-05-20 20:30:01 -0300

    Added step to merge to branch master

--- set-versions.sh
+++ set-versions.sh
@@ -10,8 +10,9 @@ function get_artifact_version_without_classifier(){
 
 function set_release_version(){
    VERSION="$(get_artifact_version_without_classifier)-RELEASE"   
+   BRANCH_NAME="$(git rev-parse --abbrev-ref HEAD)"
    echo "Generating release version : $VERSION"
-   echo "Current Branch: $(git rev-parse --abbrev-ref HEAD)"
+   echo "Current Branch: $BRANCH_NAME"
    mvn versions:set -DnewVersion=$VERSION  
    git add pom.xml
    git commit -m '[skip ci]Generating release version '$VERSION
@@ -20,11 +21,12 @@ function set_release_version(){
 
 function set_development_and_increment_version(){
    VERSION_WITHOUT_QUALIFIER="$(get_artifact_version_without_classifier)"   
+   BRANCH_NAME="$(git rev-parse --abbrev-ref HEAD)"
    a=( ${VERSION_WITHOUT_QUALIFIER//./ } )                  
    ((a[1]++))            
    VERSION="${a[0]}.${a[1]}.0-SNAPSHOT"  
    echo "Setting develop version to: $VERSION"
-   echo "Current Branch: $(git rev-parse --abbrev-ref HEAD)" 
+   echo "Current Branch:  $BRANCH_NAME" 
    mvn versions:set -DnewVersion=$VERSION
    git add pom.xml 
    git commit -m '[skip ci]Setting develop version to '$VERSION   
@@ -34,11 +36,19 @@ function set_development_and_increment_version(){
 
 function push(){
    last_tag=$(git describe --abbrev=0 --tags)
+   git checkout master
+   git merge --ff-only "$TRAVIS_COMMIT" 
    git push "https://${GITHUB_TOKEN}@github.com/fabiosoaza/spring-rest-base" master
   # git push "https://$GITHUB_TOKEN@github.com/fabiosoaza/spring-rest-base" $last_tag
    git pull
 
 }
+
+git config --global user.email 'travis@travis-ci.org'
+git config --global user.name 'Travis'
+git remote set-branches --add origin master
+git fetch
+git reset --hard
 
  case $1 in
         "develop")
