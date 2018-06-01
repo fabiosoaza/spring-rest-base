4dca8224776893f49c6ac4db24e1f91e59739bad
Fábio Souza <fabio.souza.eti@gmail.com>
2018-06-01 00:29:48 -0300

    Adding endpoint to show application version

---
+++ src/main/java/com/restbase/application/controller/InfoController.java
@@ -0,0 +1,32 @@
+package com.restbase.application.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/infos")
+public class InfoController {
+	
+	@Value("${version.number}")
+	private String versionNumber;
+	
+	public InfoController() {
+		// default constructor
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, path="version")	
+	public @ResponseBody ResponseEntity<Map<String,String>> version(){
+		Map<String,String> map = new HashMap<>();
+		map.put("version", versionNumber);
+		return ResponseEntity.status(HttpStatus.FOUND).body(map);
+	}
+
+}
